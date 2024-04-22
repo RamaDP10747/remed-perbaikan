2172eac627db8cd83d987c277991b0a182b63904
RamaDP10747 <ramadaris06@gmail.com>
2024-04-22 13:24:29 +0700

    remed prod

--- app/Http/Controllers/InboundStuffsController.php
+++ app/Http/Controllers/InboundStuffsController.php
@@ -18,7 +18,13 @@ class InboundStuffsController extends Controller
      */
     public function index()
     {
-        //
+        try {
+            $data = InboundStuff::all()->toArray();
+
+            return ApiFormatter::sendResponse(200, 'success', $data);
+        } catch (\Exception $err) {
+            return ApiFormatter::sendResponse(400, 'bad request', $err->getMessage());
+        }
     }
 
     /**
@@ -106,9 +112,20 @@ class InboundStuffsController extends Controller
      * @param  \App\Models\InboundStuffs  $inboundStuffs
      * @return \Illuminate\Http\Response
      */
-    public function show(InboundStuffs $inboundStuffs)
+    public function show($id)
     {
-        //
+        try{
+            $data = InboundStuff::where('id', $id)->first();
+
+            if (is_null($data)) {
+                return ApiFormatter::sendResponse(400, 'bad request', 'Data not found!');
+            } else {
+                return ApiFormatter::sendResponse(200, 'success', $data);
+            }
+        } catch (\Exception $err) {
+            return ApiFormatter::sendResponse (400, 'bad request', $err->getMessage());
+            }
+
     }
 
     /**
@@ -129,9 +146,33 @@ class InboundStuffsController extends Controller
      * @param  \App\Models\InboundStuffs  $inboundStuffs
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, InboundStuffs $inboundStuffs)
+    public function update(Request $request, $id)
     {
-        //
+        try {
+            $this->validate($request, [
+                'stuff_id' => 'required',
+                'total' => 'required',
+                'date' => 'required',
+                'proff_file' => 'required',
+            ]);
+
+            $checkProses = InboundStuff::where('id', $id)->update([
+                'stuff_id' => $request->stuff_id,
+                'total' => $request->total,
+                'date' => $request->date,
+                'proff_file' => $request->proff_file,
+            ]);
+
+            if($checkProses) {
+                $data = InboundStuff::find($id);
+                return ApiFormatter::sendResponse(200, 'success', $data);
+            } else {
+                return ApiFormatter::sendResponse(400, 'bad request', 'Gagal mengubah data! ');
+            }
+        } catch (\Exception $err) {
+            return ApiFormatter::sendResponse (400, 'bad request', $err->getMessage());
+            }
+
     }
 
     /**
@@ -140,8 +181,102 @@ class InboundStuffsController extends Controller
      * @param  \App\Models\InboundStuffs  $inboundStuffs
      * @return \Illuminate\Http\Response
      */
-    public function destroy(InboundStuffs $inboundStuffs)
+    public function destroy($id)
     {
-        //
+        try {
+            $checkProses = InboundStuff::where('id', $id)->delete();
+
+                return ApiFormatter::sendResponse(200, 'success', 'Data berhasil dihapus!');
+        } catch (\Exception $err) {
+            return ApiFormatter::sendResponse (400, 'bad request', $err->getMessage());
+        }
+
     }
+
+     /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\InboundStuffs  $inboundStuffs
+     * @return \Illuminate\Http\Response
+     */
+    public function trash()
+    {
+        try {
+            $data = InboundStuff::onlyTrashed()->get();
+
+                return ApiFormatter::sendResponse(200, 'success', $data);
+        } catch (\Exception $err) {
+            return ApiFormatter::sendResponse (400, 'bad request', $err->getMessage());
+        }
+
+    }
+
+     /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\InboundStuffs  $inboundStuffs
+     * @return \Illuminate\Http\Response
+     */
+    public function restore($id)
+    {
+        try {
+            $checkProses = InboundStuff::onlyTrashed()->where('id', $id)->restore();
+
+            if($checkProses) {
+                $data = InboundStuff::find($id);
+                return ApiFormatter::sendResponse(200, 'success', $data);
+            } else {
+                return ApiFormatter::sendResponse(400, 'bad request', 'Gagal mengembalikan data! ');
+            }
+        } catch (\Exception $err) {
+            return ApiFormatter::sendResponse (400, 'bad request', $err->getMessage());
+            }
+
+    }
+
+     /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\InboundStuffs  $inboundStuffs
+     * @return \Illuminate\Http\Response
+     */
+    public function deletePermanent($id)
+    {
+        try {
+            $checkProses = InboundStuff::onlyTrashed()->where('id', $id)->forceDelete();
+
+                return ApiFormatter::sendResponse(200, 'success', 'Berhasil menghapus data secara permanen!');
+        } catch (\Exception $err) {
+            return ApiFormatter::sendResponse (400, 'bad request', $err->getMessage());
+        }
+    }
+
+    public function addStock(Request $request, $id){
+        try {
+            $getStuffStock = StuffStock::find($id);
+
+            if (!$getStuffStock) {
+                return ApiFormatter::sendResponse(404, false, 'Data Stuff Stock Not Found');
+            } else {
+                $this->validate($request, [
+                    'total_available' => 'required',
+                    'total_defec' => 'required',
+                ]);
+
+                $addStock = $getStuffStock->update([
+                    'total_available' => $getStuffStock['total_available'] + $request->total_available,
+                    'total_defec' => $getStuffStock['total_defec'] + $request->total_defec,
+                ]);
+
+                if ($addStock) {
+                    $getStockAdded = StuffStock::where('id', $id)->with('stuff')->first();
+
+                    return ApiFormatter::sendResponse(200, true, 'Successfully Add A Stock Of Stuff Stock Data', $getStockAdded);
+                }
+            }
+        } catch (\Exception $err) {
+            return ApiFormatter::sendResponse (400, 'bad request', $err->getMessage());
+        }
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -13,49 +13,74 @@
 |
 */
 
-$router->get('/', function () use ($router) {
+$router->get('/', function () use($router) {
     return $router->app->version();
 });
 
-$router->get('/stuffs', 'StuffController@index');
+$router->post('/login', 'userController@login');
+$router->get('/logout', 'userController@logout');
 
-$router->post('/login', 'UserController@login');
-$router->get('/logout', 'UserController@logout');
-
-$router->group(['prefix' => 'stuff'], function() use ($router){
-    
-    $router->get('/data', 'StuffController@index');
-    $router->post('/', 'StuffController@store');
-    $router->get('/trash', 'StuffController@trash');
-
-    $router->get('{id}', 'StuffController@show');
-    $router->patch('/{id}', 'StuffController@update');
-    $router->delete('/{id}', 'StuffController@destroy');
-    $router->get('/restore/{id}', 'StuffController@restore');
-    $router->delete('/permanent/{id}', 'StuffController@deletePermanent');
+// router stuff
+$router->group(['prefix' => '/stuff', 'middleware' => 'auth'], function() use($router) {
+    // static routes
+    //  Router statis menangani URL yang memiliki pola tetap dan tidak berubah
+     $router->get('/data', 'StuffController@index'); 
+     $router->post('/', 'StuffController@store'); 
+     $router->get('/trash', 'StuffController@trash'); 
+ 
+    //dynamic routes
+    //  Router dinamis menangani URL yang dapat berubah sesuai dengan parameter atau variabel 
+    // tertentu yang diberikan
+     $router->get('{id}', 'StuffController@show'); 
+     $router->patch('{id}', 'StuffController@update'); 
+     $router->delete('{id}','StuffController@destroy'); 
+     $router->get('/trash', 'StuffController@trash'); 
+     $router->get('/restore/{id}', 'StuffController@restore'); 
+     $router->get('/permanent/{id}', 'StuffController@deletePermanent'); 
 });
 
-$router->group(['prefix' => 'user'], function() use ($router) {
-    // static routes : tetap
-    $router->post('/store', 'UserController@store');
+// router user
+$router->group(['prefix' => '/user', 'middleware' => 'auth'], function() use ($router) {
+    // static routes
+    $router->get('/data', 'UserController@index');
     $router->get('/trash', 'UserController@trash');
+    $router->post('/', 'UserController@store');
 
-    //dunamic routes : berubah - rubah
+    // dynamic routes
     $router->get('{id}', 'UserController@show');
-    $router->patch('/{id}', 'UserController@update');
-    $router->delete('/{id}','UserController@destroy');
+    $router->patch('{id}', 'UserController@update');
+    $router->delete('{id}', 'UserController@destroy');
     $router->get('/restore/{id}', 'UserController@restore');
-    $router->delete('/permanent/{id}', 'UserController@deletePermanent');
+    $router->get('/permanent/{id}', 'UserController@deletePermanent');
 });
 
-$router->group(['prefix' => 'inbound-stuffs/', 'middleware' => 'auth'], function() use ($router) {
-  
-    $router->post('store', 'InboundStuffsController@store');
-    $router->get('/', 'InboundStuffsController@index');
-    $router->get('detail/{id}', 'InboundStuffsController@show');
-    $router->patch('update/{id}', 'InboundStuffsController@update');
-    $router->delete('delete/{id}','InboundStuffsController@destroy');
-    $router->delete('recyle-bin','InboundStuffsController@recyleBin');
-    $router->get('/restore/{id}', 'InboundStuffsController@restore');
-    $router->delete('/force-delete/{id}', 'InboundStuffsController@forceDestroy');
+
+// router inboundStuff
+$router->group(['prefix' => 'inbound-stuff/', 'middleware' => 'auth'], function() use ($router) {
+    // static routes
+    $router->post('/store', 'InboundStuffController@store');
+    $router->get('/', 'InboundStuffController@index');
+    // $router->get('trash', 'InboundStuffController@trash');
+
+
+    // dynamic routes
+    $router->get('detail/{id}', 'InboundStuffController@show');
+    $router->patch('update/{id}', 'InboundStuffController@update');
+    // $router->delete('delete/{id}', 'InboundStuffController@destroy');
+    // $router->get('restore/{id}', 'InboundStuffController@restore');
+    // $router->get('deletePermanent/{id}', 'InboundStuffController@deletePermanent');
+});
+
+// router stuffStock
+$router->group(['prefix' => 'stuff-Stock', 'middleware' => 'auth'], function () use ($router) {
+    // static routes
+    $router->get('/', 'StuffStockController@index');
+    $router->get('trash', 'StuffStockController@trash');
+
+    // dynamic routes
+    $router->delete('delete/{id}', 'StuffStockController@destroy');
+    $router->get('deletePermanent/{id}', 'StuffStockController@deletePermanent');
+    $router->get('restore/{id}', 'StuffController@restore');
+    $router->post('add-stock/{id}', 'StuffStockController@addStock');
+    $router->post('sub-stock/{id}', 'StuffStockController@subStock');
 });
